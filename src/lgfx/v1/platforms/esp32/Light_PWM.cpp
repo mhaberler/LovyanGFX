0529ddeefbf01147c8ca951f586b5bc7d3f3fd8b
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-11-01 19:15:22 +0900

    tweak for ESP-IDFv5 and ArduinoESP32 v3

--- src/lgfx/v1/platforms/esp32/Light_PWM.cpp
+++ src/lgfx/v1/platforms/esp32/Light_PWM.cpp
@@ -25,6 +25,9 @@ Contributors:
  #if __has_include(<esp_arduino_version.h>)
   #include <esp_arduino_version.h>
  #endif
+ #if __has_include(<esp_arduino_version.h>)
+  #include <esp_arduino_version.h>
+ #endif
 #else
  #include <driver/ledc.h>
 #endif
@@ -43,13 +46,18 @@ namespace lgfx
 
 #if defined ( ARDUINO )
 
-    #if defined SOC_DAC_SUPPORTED // esp32-arduino core version >= 3.0.0
-        ledcAttach( _cfg.pin_bl, _cfg.freq, PWM_BITS ); // Note: auto channel ?
-    #else
-        ledcSetup(_cfg.pwm_channel, _cfg.freq, PWM_BITS);
-        ledcAttachPin(_cfg.pin_bl, _cfg.pwm_channel);
-    #endif
+#if defined ESP_ARDUINO_VERSION
+  #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+    #define LEDC_USE_IDF_V5 // esp32-arduino core 3.x.x uses the new ledC syntax
+  #endif   
+#endif
 
+#if defined LEDC_USE_IDF_V5
+    ledcAttach(_cfg.pin_bl, _cfg.freq, PWM_BITS); 
+    setBrightness(brightness);
+#else
+    ledcSetup(_cfg.pwm_channel, _cfg.freq, PWM_BITS);
+    ledcAttachPin(_cfg.pin_bl, _cfg.pwm_channel);
     setBrightness(brightness);
 #endif
 #else
